575b6b3b5a0a00330b1910396bca7fc78e6de0f8
lior-ohana <57319786+lior-ohana@users.noreply.github.com>
2020-01-13 11:55:32 +0200

    Add files via upload

--- Maagar2.py
+++ Maagar2.py
@@ -16,36 +16,55 @@ def database(user, user_type, class_managers, lecturers, students, questions, so
         questions = list2[1]
         solutions = list2[2]
         courses = list2[3]
-
+    elif user_type == "lecturer":
+        questions, solutions = main_lecturer(user, questions, solutions)
+    elif user_type == "student":
+        pass #TODO: what to do with the student.
     return [class_managers, lecturers, students, questions, solutions, courses]
 
 
 def main_class_manager(user, lecturers, questions, solutions, courses):
     choice = 0
     print("welcome to class manager menu, please insert number of the action you want to do:\n")
-    print(
-        "1.add new lecturer to the system\n 2.update or remove lecturer\n 3.add new question to the system\n 4.update or remove question\n")
-    choice = input()
-    if choice == "1":
-        lecturers = add_lecture(user, lecturers)
-    elif choice == "2":
-        lecturers = update(lecturers)
-    elif choice == "3":
-        questions, solutions = add_question(questions, solutions)
-    elif choice == "4":
-        questions, solutions = update_question(questions, solutions)
+    print("1.add new lecturer to the system\n2.update or remove lecturer\n3.add new question to the system\n"
+          "4.update or remove question\n5.exit\n")
+    while choice != "5":
+        choice = input()
+        if choice == "1":
+            lecturers = add_lecture(user, lecturers)
+        elif choice == "2":
+            lecturers = update(lecturers)
+        elif choice == "3":
+            questions, solutions = add_question(questions, solutions)
+        elif choice == "4":
+            questions, solutions = update_or_delete_question(questions, solutions)
+        elif choice == "5":
+            print("Goodbye\n")
+        else:
+            print("invalid input, try again.\n")
+        print("You're back in the menu. Please choose:\n1.add new lecturer to the system\n2.update or remove lecturer\n"
+              "3.add new question to the system\n""4.update or remove question\n5.exit\n")
     return [lecturers, questions, solutions, courses]
 
-def main_lecturer(user, questions, solutions):
-    print("welcome to class manager menu, please insert number of the action you want to do:\n")
-    print("1.add new question\n 2.update or remove question info\n")
-    choice = input()
 
-    if choice == "1":
-        questions, solutions = add_question(questions, solutions)
-    elif choice == "2":
-        questions, solutions = update_question(questions, solutions)
+def main_lecturer(user, questions, solutions):
+    choice = 0
+    print("welcome to class manager menu, please insert number of the action you want to do:\n")
+    print("1.add new question\n2.update or remove question info\n3.exit\n")
+    while choice != "3":
+        choice = input()
+        if choice == "1":
+            questions, solutions = add_question(questions, solutions)
+        elif choice == "2":
+            questions, solutions = update_or_delete_question(questions, solutions)
+        elif choice == "3":
+            print("Goodbye\n")
+        else:
+            print("invalid input, try again.\n")
+        print("You're back in the menu. Please choose:\n1.add new question\n2.update or remove question info\n3.exit\n")
     return [questions, solutions]
+
+
 # database will return a list of all the lists to the main, for updating the files.
 
 
@@ -69,7 +88,7 @@ def add_lecture(user, lecturers):
     new_lecture.department = user.department
 
     new_lecture.list_courses = []
-    while (name_course != "-1"):
+    while name_course != "-1":
         print("insert courses for the lecture ; if you want to exit press -1\n")
         name_course = input()
         if name_course != "-1":
@@ -97,8 +116,8 @@ def update(lecturers):
             for x in lecturers:
 
                 if x.name == name.capitalize():
-                    print(
-                        "what do you want to update:\n 1.name\n 2.password\n 3.phone number\n 4.department\n 5.courses\n")
+                    print("what do you want to update:\n 1.name\n 2.password\n "
+                          "3.phone number\n 4.department\n 5.courses\n")
                     choice2 = input()
                     if choice2 == "1":
                         print("insert new name for lecturer:\n")
@@ -119,8 +138,8 @@ def update(lecturers):
                     elif choice2 == "5":
                         exit1 = False
                         while (exit1 == False):
-                            print(
-                                "press 1-if you want to delete course\n 2-if you want to add course\nor press -1 to exit\n")
+                            print("press 1-if you want to delete course\n"
+                                  " 2-if you want to add course\n or press -1 to exit\n")
                             choice3 = input()
                             if choice3 == "1":
                                 print("Enter the name of the course you want to remove\n")
@@ -167,6 +186,7 @@ def add_solution(code_q, list_solutions):
 def add_question(list_questions, list_solutions):
     found = True
     ques = None
+    code = 0
     while not found:
         code = random.randint(0, 100000)
         for i in list_questions:
@@ -212,75 +232,102 @@ def add_question(list_questions, list_solutions):
     return [list_questions, ls]
 
 
-def update_question(list_questions, list_solutions):
+def update_or_delete_question(list_questions, list_solutions):
     choice = 0
-    print("insert your choice for action:\n")
-    print("1.update question info\n 2.remove question from system\n")
-    choice = input()
     code = 0
     temp = ""
+    while choice != "3":
+        print("insert your choice for action:\n")
+        print("1.update question info\n 2.remove question from system\n 3. exit")
+        choice = input()
+        if choice == "1":
+            found = False
+            while not found:
+                print("insert question code for update\nwrite \"exit\" to exit")
+                code = input()
+                if code.lower() == "exit":
+                    break
+                for x in list_questions:
+                    if x.code_q == code:
+                        found = True
+                        print("what do you want to update?\n 1.subject\n 2.sub subject\n"
+                              " 3.level\n 4.type test\n 5.year\n 6.semester\n 7.term test\n 8.format\n 9.solution\n")
+                        choice = input()
+                        if choice == "1":
+                            print("insert new subject for question\n")
+                            temp = input()
+                            x.subject = temp
+                        elif choice == "2":
+                            print("insert new sub subject\n")
+                            temp = input()
+                            x.sub_subject = temp
+                        elif choice == "3":
+                            print("insert new level for question\n")
+                            temp = input()
+                            if temp >= "1" and temp <= "10":
+                                x.level = temp
+                            else:
+                                print("Error! level must be 1-10!\n")
+                        elif choice == "4":
+                            print("insert new type test for question\n")
+                            temp = input()
+                            x.type_test = temp
+                        elif choice == "5":
+                            print("insert new year for question\n")
+                            temp = input()
+                            x.year = temp
+                        elif choice == "6":
+                            print("insert new semester for question\n")
+                            temp = input()
+                            x.semester = temp
+                        elif choice == "7":
+                            print("insert new term test for question\n")
+                            temp = input()
+                            x.term_test = temp
+                        elif choice == "8":
+                            print("insert new format for question\n")
+                            temp = input()
+                            x.format_q = temp
+                        elif choice == "9":
+                            if x.is_solution == False:
+                                add_solution(x.code_q, list_solutions)
+                            else:
+                                exit2 = False
+                                while exit2 == False:
+                                    print("this question already has a solution\n"
+                                          " insert 1 to remove solution\n insert -1 to Exit\n")
+                                    choice = input()
+                                    if choice == "1":
+                                        print("insert question code to remove solution:\n")
+                                        code = input()
+                                        for y in list_questions:
+                                            if y.code_q == code:
+                                                list_solutions.remove(y)
+                                                y.is_solution = False
+                                    elif choice == "-1":
+                                        exit2 = True
+                        else:
+                            print("matching code was not found it the system\n ")
+                if not found:
+                    print("No question with that code. please try again")
+        elif choice == "2":
+            list_questions, list_solutions = delete_question(list_questions, list_solutions)
+        elif choice != "3":
+            print("invalid input")
+            # if choise == "3" will end the while and return
+    return [list_questions, list_solutions]
 
-    if choice == "1":
-        print("insert question code for update\n")
-        code = input()
-        for x in list_questions:
-            if x.code_q == code:
-                print(
-                    "what do you want to update?\n 1.subject\n 2.sub subject\n 3.level\n 4.type test\n 5.year\n 6.semester\n 7.term test\n 8.format\n 9.solution\n")
-                choice = input()
-                if choice == "1":
-                    print("insert new subject for question\n")
-                    temp = input()
-                    x.subject = temp
-                elif choice == "2":
-                    print("insert new sub subject\n")
-                    temp = input()
-                    x.sub_subject = temp
-                elif choice == "3":
-                    print("insert new level for question\n")
-                    temp = input()
-                    if temp >= "1" and temp <= "10":
-                        x.level = temp
-                    else:
-                        print("Error! level must be 1-10!\n")
-                elif choice == "4":
-                    print("insert new type test for question\n")
-                    temp = input()
-                    x.type_test = temp
-                elif choice == "5":
-                    print("insert new year for question\n")
-                    temp = input()
-                    x.year = temp
-                elif choice == "6":
-                    print("insert new semester for question\n")
-                    temp = input()
-                    x.semester = temp
-                elif choice == "7":
-                    print("insert new term test for question\n")
-                    temp = input()
-                    x.term_test = temp
-                elif choice == "8":
-                    print("insert new format for question\n")
-                    temp = input()
-                    x.format_q = temp
-                elif choice == "9":
-                    if x.is_solution == False:
-                        add_solution(x.code_q, list_solutions)
-                    else:
-                        exit2 = False
-                        while (exit2 == False):
-                            print(
-                                "this question already has a solution\n insert 1 to remove solution\n insert -1 to Exit\n")
-                            choice = input()
-                            if choice == "1":
-                                print("insert question code to remove solution:\n")
-                                code = input()
-                                for y in list_questions:
-                                    if y.code_q == code:
-                                        list_solutions.remove(y)
-                                        y.is_solution = False
-                            elif choice == "-1":
-                                exit2 = True
-                else:
-                    print("matching code was not found it the system\n ")
+
+def delete_question(list_questions, list_solutions):
+    print("insert question code to delete\n")
+    code = input()
+    for x in list_questions:
+        if x.code_q == code:
+            list_questions.remove(x)
+            # delete solution if exist
+            for s in list_solutions:
+                if s.code_q == code:
+                    list_solutions.remove(s)
+            print("question was deleted")
+            break
     return [list_questions, list_solutions]
