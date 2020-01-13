a285eb39ef0c4becc53ebb36fa705bc85de19315
lior-ohana <57319786+lior-ohana@users.noreply.github.com>
2020-01-13 12:06:55 +0200

    Delete LogWriter.py

--- LogWriter.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-
-
-def write_log(time, run_time, action):
-    f = open("LOG.txt", "a")
-    f.write(str(time) + "RUN-TIME: " + str(run_time) + " LOG: " + str(action) + "\n")
-    f.close()
